39079ace1907ea20d22de37940a85419c32654a5
dev-Iamsaurabh <sau4478@gmail.com>
2022-11-09 06:39:47 +0530

    Reuasable navbar and footer

---
+++ components/fot.js
@@ -0,0 +1,71 @@
+function fot(){
+    return `<div id="footer_child">
+
+    <div class="footer_item">
+        <h4>Company</h4>
+        <a>Home</a>
+        <a>Terms</a>
+        <a>Privacy</a>
+        <a>Cookies</a>
+        <a>Affiliate terms</a>
+        <a>Download app</a>
+        <a>Pricing</a>
+        <a>Affiliate</a>
+        <a>Press</a>
+        <a>DeskTime in your language</a>
+    </div>
+    <div class="footer_item">
+        <h4>Learn more</h4>
+        <a>All resources</a>
+        <a>Employee monitoring guide</a>
+        <a>Online employee time clock</a>
+        <a>DeskTime for business</a>
+        <a>DeskTime for freelancers</a>
+        <a>FAQ</a>
+        <a>Case studies</a>
+        <a>Webinars</a>
+        <a>Blog</a>
+    </div>
+    <div class="footer_item">
+        <h4>Integrations</h4>
+        <a>All integrations</a>
+        <a>Trello</a>
+        <a>Basecamp</a>
+        <a>Jira</a>
+        <a>Asana</a>
+        <a>Outlook Calendar</a>
+        <a>Google Calendar</a>
+    </div>
+    <div class="footer_item">
+        <h4>Features</h4>
+        <a>All features</a>
+        <a>Automatic time tracking</a>
+        <a>URL & App tracking</a>
+        <a>Screenshots</a>
+        <a>Project time tracking</a>
+        <a>Shift schedule</a>
+        <a>Offline time tracking</a>
+        <a>Absence calendar</a>
+        <a>Mobile app</a>
+        <a>Feature request</a>
+
+    </div>
+    <div class="footer_item">
+        <h4>Help center</h4>
+        <a>Contact us</a>
+        <a>Schedule a call</a>
+        <a>Send us an e-mail</a>
+        <a>Request In-person training</a>
+        <a>Open chat</a>
+        <a>Phone support</a>
+        <a>+1 (315) 6365354</a>
+        <a>MON-FRI 9:00-22:00 EET</a>
+    </div>
+
+</div>
+
+
+<img src="/assets/footer_desktime.png" alt="">`
+}
+
+export default fot
---
+++ scripts/navbar.js
@@ -0,0 +1,12 @@
+setTimeout(() => {
+
+
+    // do your all navbar functionalities here 
+
+    document.querySelector("#logo").onclick=()=>{
+        window.location.href="../index.html"
+    }
+
+
+    
+}, 100);
