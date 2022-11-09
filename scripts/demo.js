d9d9468e0e05df3aa8d7d7c1e527039d9c695860
shaifali <shaifali02000@gmail.com>
2022-11-09 19:30:52 +0530

    demo page done

---
+++ scripts/demo.js
@@ -0,0 +1,19 @@
+//import navbar
+import nav from '/components/nav.js';
+
+let navbar=document.getElementById('navbar');
+navbar.innerHTML=nav();
+
+//import footer
+import fot from '/components/fot.js';
+
+let footer=document.getElementById('footer');
+footer.innerHTML=fot();
+
+// <!-- Demo 4 cards -->
+
+
+
+  
+
+
