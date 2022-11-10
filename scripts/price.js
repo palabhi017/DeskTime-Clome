a142e804ce8730d0482a9c8bbe12830770b9772f
LokeshGola <lokeshgola121@gmail.com>
2022-11-10 20:39:17 +0530

    popup funtion

--- scripts/price.js
+++ scripts/price.js
@@ -4,6 +4,23 @@ document.getElementById("navbar").innerHTML=nav();
 import fot from "../components/fot.js";
 document.querySelector("#footer").innerHTML=fot()
 
+const monthly_btn=document.getElementById("monthly")
+monthly_btn.onclick=()=>{
+    const costs =document.querySelectorAll(".cost");
+    let costArr=[560,860,1600];
+    costs.forEach((el,i)=> {
+        el.innerText=costArr[i];       
+    })
+}
+const annual_btn=document.getElementById("annual")
+annual_btn.onclick=()=>{
+    const costs =document.querySelectorAll(".cost");
+    let costArr=[513.33,789.33,1466.67];
+    costs.forEach((el,i)=> {
+        el.innerText=costArr[i];       
+    })
+}
+
 const basicfeatures=["Automatic time tracking","URL and app tracking","Integrated web timer","Productivity calculation","Idle time tracking","Project time tracking","Mobile app","User integrations"];
 const proplus=["Automatic screenshots","Absence calendar","Shift Scheduling","Booking","Invoicing","Company integrations","Offline time approval","IP restrictions"]
 const premiumplus=["VIP support","Personalized onboarding","Unlimited projects","Unlimited tasks","Unlimited data history","Custom API functions"]
